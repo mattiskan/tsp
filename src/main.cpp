80183d8372f32deb84d23e509569c3c6b8fb524b
Axel Lewenhaupt <axel@x2d.org>
2013-12-05 20:51:49 +0100

    added print solution

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@ Points points;
 
 void read();
 void printPoints();
+void printSolution(std::vector<Point*> & solution);
 
 int main(){
   read();
@@ -29,3 +30,8 @@ void printPoints() {
     printf("(%f, %f)\n", (*it)->x, (*it)->y);
 }
 
+void printSolution(std::vector<Point*> & solution) {
+  for (auto it=solution.begin(); it<solution.end(); ++it) {
+    printf("%d\n", (*it)->i);
+  }
+}
